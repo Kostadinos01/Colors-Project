1b6170f5126e4140eddcb991a5255b46b110fe1a
Kostadinos01 <kostasigna@yahoo.gr>
2022-02-08 15:03:51 +0200

    git commit

---
+++ src/Palette.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import NavBar from "./NavBar";
+import "./Palette.css";
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500, format: "hex" };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
+
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+
+  render() {
+    const { colors, paletteName, id } = this.props.palette;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        id={color.id}
+        paletteId={id}
+        showLink
+      />
+    ));
+    return (
+      <div className="Palette">
+        <NavBar level={level} changeLevel={this.changeLevel} />
+        <div className="Palette-colors">{colorBoxes}</div>
+        <footer className="Palette-footer">{paletteName}</footer>
+      </div>
+    );
+  }
+}
+
+export default Palette;
